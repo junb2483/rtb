ca4183de0b1dfa5446f797c56fd6a6dce7961814
Duong Tran <duongtran@everfit.io>
2024-08-17 22:35:33 +0700

    feat: update test page

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,49 +1,19 @@
-const CACHE_NAME = "my-cache-v1";
-const CACHE_DURATION = 3600 * 1000; // 1 tiếng (3600 giây)
-
-// Cài đặt Service Worker và cache file
+// service-worker.js
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.add("/rtb/test");
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll([
+        "/index.html",
+        // Danh sách các tài nguyên cần lưu cache khác (nếu có)
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-// Xử lý fetch để cung cấp file từ cache hoặc từ mạng
 self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-
-  // Chỉ cache và trả về file từ đường dẫn không có query params
-  console.log(1, url.pathname);
-  if (url.pathname === "/rtb/test") {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(async (cache) => {
-        const cachedResponse = await cache.match(url.pathname);
-        const fetchPromise = fetch(url.pathname).then((networkResponse) => {
-          if (networkResponse && networkResponse.status === 200) {
-            cache.put(url.pathname, networkResponse.clone());
-          }
-          return networkResponse;
-        });
-
-        if (cachedResponse) {
-          const cachedTime = new Date(
-            cachedResponse.headers.get("date")
-          ).getTime();
-          const currentTime = Date.now();
-
-          // Kiểm tra thời gian cache
-          if (currentTime - cachedTime < CACHE_DURATION) {
-            return cachedResponse || fetchPromise;
-          }
-        }
-
-        return fetchPromise.catch(() => {
-          return caches.match(url.pathname);
-        });
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
