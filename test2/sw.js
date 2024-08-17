2153e8bff34a8bec88e9fc81e149c4c3cc8ca839
Duong Tran <duongtran@everfit.io>
2024-08-17 23:13:16 +0700

    feat: update test page

---
+++ test2/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "my-cache-v5";
+const urlsToCache = ["/", "/rtb/test"];
+
+// Cài đặt cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Xử lý các yêu cầu và sử dụng cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request, { ignoreSearch: true }) // Bỏ qua phần truy vấn của URL
+      .then((response) => {
+        // Trả về dữ liệu từ cache nếu có
+        if (response) {
+          return response;
+        }
+        // Nếu không có dữ liệu trong cache, gửi yêu cầu mạng
+        return fetch(event.request);
+      })
+  );
+});
